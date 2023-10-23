9fd81b668a673b91226fc3c4bbd2b5cb9a395033
pformela <patrykformela.00@gmail.com>
2023-10-23 09:50:17 +0200

    finished assignment 2

---
+++ assignment2/src/app/user/user.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss'],
+})
+export class UserComponent {
+  public username: string = '';
+
+  public onResetUsername(): void {
+    this.username = '';
+  }
+}
