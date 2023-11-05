b23fd85c1616d712f365cc023eea1ed5645978da
pformela <patrykformela.00@gmail.com>
2023-11-05 20:43:38 +0100

    added styling and layouts for all elements

--- course-project/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  public recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'This is simply a test',
+      'https://www.southernliving.com/thmb/HSEUOjJVCl4kIRJRMAZ1eblQlWE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg'
+    ),
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
